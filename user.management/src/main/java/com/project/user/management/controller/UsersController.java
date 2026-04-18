3f9bb966929930e6a166a24ea59b25919f55e0bb
Suraj <mehersuraj040@gmail.com>
2026-04-18 08:35:24 +0530

    fix controller

--- user.management/src/main/java/com/project/user/management/controller/UsersController.java
+++ user.management/src/main/java/com/project/user/management/controller/UsersController.java
@@ -2,6 +2,7 @@ package com.project.user.management.controller;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,38 +12,41 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.project.user.management.dto.UsersRequestDto;
 import com.project.user.management.dto.UsersResponseDto;
-import com.project.user.management.entity.Users;
+import com.project.user.management.dto.UsersUpdateRequestDto;
 import com.project.user.management.service.UsersService;
 
 @RestController
 public class UsersController {
 
-	UsersService service;
+    private final UsersService service;
 
-	public UsersController(UsersService service) {
-		super();
-		this.service = service;
-	}
-	
-	@PostMapping("/register")
-	public String register(@RequestBody UsersRequestDto user) {
-		return service.register(user);
-	}
-	@GetMapping("/searchUser/{id}")
-	public UsersResponseDto searchUser(@PathVariable Long id) {
-		return service.searchUser(id);
-	}
-	@GetMapping("/viewAllUsers")
-	public List<UsersResponseDto> viewAllUsers(){
-		return service.viewAllUsers();
-	}
-	@PutMapping("/updateUser")
-	public String updateUser(@RequestBody Users user) {
-		return service.updateUser(user);
-	}
-	@GetMapping("/deleteUser/{id}")
-	public String deleteUser(@PathVariable Long id) {
-		return service.deleteUser(id);
-	}
-	
+    public UsersController(UsersService service) {
+        super();
+        this.service = service;
+    }
+
+    @PostMapping("/register")
+    public String register(@RequestBody UsersRequestDto user) {
+        return service.register(user);
+    }
+
+    @GetMapping("/searchUser/{id}")
+    public UsersResponseDto searchUser(@PathVariable Long id) {
+        return service.searchUser(id);
+    }
+
+    @GetMapping("/viewAllUsers")
+    public List<UsersResponseDto> viewAllUsers() {
+        return service.viewAllUsers();
+    }
+
+    @PutMapping("/updateUser")
+    public UsersResponseDto updateUser(@RequestBody UsersUpdateRequestDto userDto) {
+        return service.updateUser(userDto);
+    }
+
+    @DeleteMapping("/deleteUser/{id}")
+    public String deleteUser(@PathVariable Long id) {
+        return service.deleteUser(id);
+    }
 }
